497bc294798fa333ba18a86fe90985c23416d947
qantus <qantus@mail.ru>
2014-06-28 18:04:11 +0400

    First commit

---
+++ Daemon/Data.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLite;
+using System.Globalization;
+
+namespace Daemon
+{
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(255)]
+        public string FirstName { get; set; }
+        [MaxLength(255)]
+        public string MiddleName { get; set; }
+        [MaxLength(255)]
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool Print { get; set; }
+
+        // Statistic
+        public int TotalAccounts { get; set; }
+        public int ActiveAccounts { get; set; }
+
+        public long HighCredit { get; set; }
+        public long SheduledPayments { get; set; }
+        public long CurrentBalance { get; set; }
+        public long PastDueBalance { get; set; }
+        public long OutstandingBalance { get; set; }
+
+        public DateTime RecentAcc { get; set; }
+        public DateTime OldestAcc { get; set; }
+
+        public int TotalRequests { get; set; }
+        public int RecentRequests { get; set; }
+        public int LongRequests { get; set; }
+
+        public string RecentRequest { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format ("{0} {1} {2}", FirstName, MiddleName, LastName);
+        }    
+    }
+
+    public class Request
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [Indexed]
+        public int UserId { get; set; }
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        public long Sum { get; set; }
+        public DateTime Date { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1}", Name, Sum);
+        }
+    }
+
+    public class Database : SQLiteConnection
+    {
+        public Database(string path) : base(path)
+        {
+            CreateTable<User>();
+            CreateTable<Request>();
+        }
+
+        public IEnumerable<User> QueryAllUsers()
+        {
+            return from s in Table<User>()
+                   select s;
+        }
+
+        public IEnumerable<User> QueryUsersPrint()
+        {
+            return from s in Table<User>()
+                   where s.Print == true
+                   select s;
+        }
+
+        // @TODO: Birth date check
+        public User QueryUser(string FirstName, string MiddleName, string LastName)
+        {
+            return (from s in Table<User>()
+                    where s.FirstName == FirstName && s.MiddleName == MiddleName && s.LastName == LastName 
+                    select s).FirstOrDefault();
+        }
+
+        public User GetOrCreateUser(string FirstName, string MiddleName, string LastName)
+        {
+            var user = this.QueryUser(FirstName, MiddleName, LastName);
+            if (user == null)
+            {
+                user = new User();
+                user.FirstName = FirstName;
+                user.MiddleName = MiddleName;
+                user.LastName = LastName;
+            }
+            return user;
+        }
+
+        public void save(object obj)
+        {
+            if (this.InDatabase(obj))
+            {
+                this.Update(obj);
+            }
+            else 
+            {
+                this.Insert(obj);
+            }
+        }
+
+        public bool InDatabase(object obj)
+        { 
+            var type = obj.GetType();
+            var map = GetMapping(type);
+            var pk = map.PK;
+
+            var found = this.Find(pk.GetValue(obj), map);
+            return found != null;
+        }
+    }
+}
---
+++ Daemon/Parser.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml.Linq;
+using SQLite;
+using System.Globalization;
+
+namespace Daemon
+{
+    class Parser
+    {
+        Database _db;
+        public string targetDir = "C:\\Daemon\\data\\";
+        public string outputStream = "C:\\Daemon\\output.txt";
+        public string dbPath = "C:\\Daemon\\db.db";
+
+        public Database db()
+        {
+            if (this._db == null) {
+                this._db = new Database(this.dbPath);
+            }
+            return this._db;
+        }
+
+        public void Parse()
+        {
+            string[] fileEntries = Directory.GetFiles(this.targetDir);
+            foreach (string fileName in fileEntries)
+                this.ProcessFile(fileName);
+            this.writeData();
+        }
+
+        public void ProcessFile(string fileName)
+        {
+            if (fileName.EndsWith(".xml"))
+            {
+                this.processXML(fileName);
+            }
+            //else if (fileName.EndsWith(".html"))
+            //{
+            //    this.processHTML(fileName);
+            //}
+        }
+
+        public DateTime DateFromString(string DateString, string format = "yyyy-MM-ddzzz")
+        {
+            const DateTimeStyles style = DateTimeStyles.AllowWhiteSpaces;
+            DateTime result = new DateTime();
+            DateTime dt;
+            if (DateTime.TryParseExact(DateString, format, CultureInfo.InvariantCulture, style, out dt))
+                result = dt;
+            return result;   
+        }
+
+        public int IntFromString(string IntString)
+        {
+            try
+            {
+                return Convert.ToInt32(IntString);
+            }
+            catch (Exception e) { }
+            return 0;
+        }
+
+        public Int64 LongFromString(string IntString)
+        {
+            try
+            {
+                return Convert.ToInt64(IntString);
+            }
+            catch (Exception e) { }
+            return 0;
+        }
+
+        public void processXML(string fileName)
+        {
+            Database db = this.db();
+            User user = null;
+            XDocument xDocument = new XDocument();
+
+            try
+            {
+                xDocument = XDocument.Load(fileName);
+                var rawUser = this.XMLGetUser(xDocument, db);
+                user = rawUser;
+            }
+            catch (Exception e) { };
+
+            if (user != null)
+            {
+                try
+                {
+                    user = this.XMLFillStatisticInfo(user, xDocument);
+                }
+                catch (Exception e) { };
+                db.save(user);
+            }
+
+        }
+
+        public User XMLGetUser(XDocument xDocument, Database db)
+        { 
+            var PersonReq = xDocument.Descendants("PersonReq");
+            if (PersonReq.Count() > 0)
+            {
+                var person = PersonReq.ElementAt(0);
+                var FirstName = person.Element("first").Value;
+                var MiddleName = person.Element("paternal").Value;
+                var LastName = person.Element("name1").Value;
+                var BirthDate = this.DateFromString(person.Element("birthDt").Value);
+                if (FirstName != "" && LastName != "")
+                {
+                    var user = db.GetOrCreateUser(FirstName, MiddleName, LastName);
+                    user.Print = true;
+                    user.BirthDate = BirthDate;
+                    return user;
+                }
+            }
+
+            return null;
+        }
+
+        public User XMLFillStatisticInfo(User user, XDocument xDocument) 
+        {
+            var CalcRaw = xDocument.Descendants("calc");
+            if (CalcRaw.Count() > 0)
+            {
+                var person = CalcRaw.ElementAt(0);
+
+                user.TotalAccounts = this.IntFromString(person.Element("totalAccts").Value);
+                user.ActiveAccounts = this.IntFromString(person.Element("totalActiveBalanceAccounts").Value);
+
+                user.HighCredit = this.XMLNestedLongFetch(person, "totalHighCredit", "Value");
+                user.CurrentBalance = this.XMLNestedLongFetch(person, "totalCurrentBalance", "Value");
+                user.PastDueBalance = this.XMLNestedLongFetch(person, "totalPastDueBalance", "Value");
+                user.OutstandingBalance = this.XMLNestedLongFetch(person, "totalOutstandingBalance", "Value");
+                user.SheduledPayments = this.XMLNestedLongFetch(person, "totalScheduledPaymnts", "Value");
+
+                user.TotalRequests = this.IntFromString(person.Element("totalInquiries").Value);
+                user.RecentRequests = this.IntFromString(person.Element("recentInquiries").Value);
+                user.LongRequests = this.IntFromString(person.Element("collectionsInquiries").Value);
+                user.RecentRequest = person.Element("mostRecentInqText").Value;
+                user.RecentAcc = this.DateFromString(person.Element("mostRecentAcc").Value);
+                user.OldestAcc = this.DateFromString(person.Element("oldest").Value);
+            }
+            return user;
+        }
+
+        public long XMLNestedLongFetch(XElement xElement, string FirstLevel, string SecondLevel)
+        {
+            var firstLevel = xElement.Element(FirstLevel);
+            if (firstLevel != null)
+            {
+                var secondLevel = firstLevel.Element(SecondLevel);
+                if (secondLevel != null)
+                {
+                    return this.LongFromString(secondLevel.Value);
+                }
+            }
+            return 0;
+        }
+        //public void processHTML(string fileName)
+        //{
+        //    Dictionary<string, object> data = new Dictionary<string, object>();
+        //    HtmlAgilityPack.HtmlDocument htmlDoc = new HtmlAgilityPack.HtmlDocument();
+        //    htmlDoc.OptionFixNestedTags = true;
+        //    htmlDoc.Load(fileName);
+        //    data.Add(fileName, htmlDoc.DocumentNode.InnerHtml); 
+        //}
+
+        public void writeData()
+        {
+            var db = this.db();
+
+            System.IO.StreamWriter file = new System.IO.StreamWriter(this.outputStream);
+
+            foreach (User user in db.QueryUsersPrint())
+            {
+                file.WriteLine(user.FirstName);
+                //foreach (KeyValuePair<string, object> variable in fileData.Value)
+                //{
+                //    file.WriteLine(variable.Key);
+                //    file.WriteLine(variable.Value);
+                //}
+            }
+            file.Close();
+        }
+    }
+}
